6a345dbfafd025d94207afbd5786d9991541251c
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-05-04 12:00:49 -0400

    Add files via upload

---
+++ Accordian/data.jsx
@@ -0,0 +1,29 @@
+const data = [
+{
+    id : '1',
+    question : 'What are accordian component?',
+    answer :
+      'Accordian components are user interface elements used for organzing and presenting code.'
+},
+{
+    id : '2',
+    question : 'What are they used for?',
+    answer :
+      'They are commonly employed in various contexts, including FAQs, product descriptions, and more.'
+},
+{
+    id : '3',
+    question : "Isn't an Accordian a musical instrument?",
+    answer :
+      'Yes, the Accordian is a musical instrument with a keyboard and bellows, producing a unique sound.'
+},
+{
+    id : '4',
+    question : 'Should I practice making an Accordian Component?',
+    answer :
+      'Yes, I would highly recommend building an accordian for practice.'
+},
+
+];
+
+export default data;
---
+++ App.js
@@ -0,0 +1,12 @@
+import Accordian from './components/Accordian/Accordian';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Accordian />
+    </>
+  );
+}
+
+export default App;
